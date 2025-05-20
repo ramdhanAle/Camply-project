9433760bec410eab7bc368bf08995491edfb59e6
ramdhanAle <leeslowly32@gmail.com>
2025-05-20 16:23:20 +0700

    otorisasi backend

--- app/Http/Middleware/AdminOnly.php
+++ app/Http/Middleware/AdminOnly.php
@@ -8,10 +8,10 @@ use Illuminate\Http\Request;
 class AdminOnly
 {
     public function handle($request, Closure $next)
-{
+    {
     if (auth()->user()->role !== 'admin') {
         abort(403, 'Hanya admin yang boleh akses');
     }
     return $next($request);
-}
+    }
 }
